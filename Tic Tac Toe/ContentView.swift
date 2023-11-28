2a8a3b625f692ee1ce81f9c8e71a440659d6c9eb
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-11-28 16:04:51 -0600

    Created LazyVGrid

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            
             Text("Tic Tac Toe")
                 .font(.title).bold()
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed (120), spacing: 15), count: 3), spacing: 15) {
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
+                Text("X")
                 
+            }
         }
         .preferredColorScheme(.dark)
         .padding()
