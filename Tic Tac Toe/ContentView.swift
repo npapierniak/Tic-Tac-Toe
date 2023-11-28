4de2812af51c4fcb7a48e401293c1daae59e25ba
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-11-28 17:10:29 -0600

    Added checkForWinner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -19,7 +21,7 @@ struct ContentView: View {
                     ZStack {
                         Color.blue
                         Color.white
-                        .opacity (moves [index] == "" ? 1 : 0)
+                            .opacity (moves [index] == "" ? 1 : 0)
                         Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
@@ -36,13 +38,25 @@ struct ContentView: View {
                     }
                     .rotation3DEffect(
                         .init(degrees: moves [index] != "" ? 180: 0),
-                    axis: (x: 0.0, y: 1.0, z: 0.0))
-
+                        axis: (x: 0.0, y: 1.0, z: 0.0))
                 }
             }
+            
         }
         .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver){
+                Alert(title: Text (winMessage))
+            }
+        .onChange(of: moves) {
+            newValue in checkForWinner()
+        }
         .padding()
+    }
+    private func checkForWinner() {
+        if moves [0] != "" && moves [0] == moves [1] && moves [1] == moves [2] {
+            winMessage = "\(moves[0]) is the winner!"
+            gameOver = true
+        }
     }
 }
 
